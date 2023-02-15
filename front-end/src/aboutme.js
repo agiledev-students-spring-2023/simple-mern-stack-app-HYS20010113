87362731b0433361d335ef9ee2d114b6e1096087
lijunrui <sherlockli0222@163.com>
2023-02-14 22:48:15 -0500

    edit photograph

--- front-end/src/aboutme.js
+++ front-end/src/aboutme.js
@@ -1,5 +1,5 @@
 import './Header.css'
-import me from './me.jpeg'
+import me from './wjq.jpg'
 
 /**
  * A React component that is used for the header displayed at the top of every page of the site.
@@ -12,7 +12,7 @@ const Aboutme = props => {
     <h1>Hello!</h1>
     <p>My name in Junrui Li, currently a junior at NYU majoring in Mathematics and Computer Science. </p>
     <p>I am interested in playing CS:GO and watch relative tournament. I am also a big fan of MachineWJQ who streams at www.douyu/6657.com</p>
-    <img src={me} style={{width:100}} alt='My Photo'></img>
+    <img src={me} style={{width:200}} alt='My Photo'></img>
     </>
   )
 }
